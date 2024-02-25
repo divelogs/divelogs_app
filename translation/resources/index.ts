b75ce99b459c41973fb64017ad64cd65d1fabf3f
Rainer Mohr <mail@klemmkeil.de>
2024-02-25 19:52:31 +0100

    dutch translation

--- translation/resources/index.ts
+++ translation/resources/index.ts
@@ -2,3 +2,4 @@ export { default as en } from "./en";
 export { default as de } from "./de";
 export { default as fr } from "./fr";
 export { default as it } from "./it";
+export { default as nl } from "./nl";
